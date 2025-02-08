e95eb06accc4d35e79ec53117aef9dd4470aeff6
Heverton Reis <heverton.reis04@gmail.com>
2025-02-08 19:49:16 -0300

    doc: Adicionando pipeline completa

--- Quartus/IF_STAGE.v
+++ Quartus/IF_STAGE.v
@@ -1,60 +1,84 @@
-module IF_Stage (
-    input wire clk,                       // Clock do sistema para sincronização
-    input wire rst,                  // Sinal de reset
-    input wire [31:0] branchTarget,      // Endereço calculado em caso de desvio (branch)
-    input wire BranchTaken,              // Indica se o branch foi tomado
-    input wire PCSrc,                    // Controla a seleção do próximo valor do PC
-    input wire PCWrite,                  // Habilita a leitura da memória de instrução
-    input wire IF_ID_WriteEnable,        // Habilita a escrita no registrador de pipeline IF/ID
-    output wire [31:0] IF_ID_PC,         // Endereço do PC para o próximo estágio (ID)
-    output wire [31:0] IF_ID_Instruction // Instrução buscada na memória para o próximo estágio (ID)
+// Copyright (C) 2018  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
+// and other software and tools, and its AMPP partner logic 
+// functions, and any output files from any of the foregoing 
+// (including device programming or simulation files), and any 
+// associated documentation or information are expressly subject 
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details.
+
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition"
+// CREATED		"Sat Feb 08 13:59:02 2025"
+
+module IF_STAGE(
+	clk,
+	rst,
+	BranchTaken,
+	IF_ID_WriteEnable,
+	clk_ROM,
+	BranchTarget,
+	IF_ID_Instruction,
+	IF_ID_PC
 );
 
-    // Declaração de sinais internos
-    wire [31:0] currentPC;       // Armazena o valor atual do PC
-    wire [31:0] nextPC;          // Armazena o próximo valor calculado do PC
-    wire [31:0] incrementedPC;   // Valor do PC incrementado (próximo endereço sequencial)
-    wire [31:0] instruction;     // Instrução lida da memória
 
-    // Contador de Programa (PC)
-    // Módulo responsável por armazenar o endereço da próxima instrução.
-    // O valor do PC é atualizado com base em 'nextPC' se 'PCWrite' for ativo.
-    ProgramCounter pc (
-        .clk(clk),
-        .rst(rst),
-        .nextPC(nextPC),
-        .PCWrite(PCWrite),
-        .currentPC(currentPC)
-    );
+input wire	clk;
+input wire	rst;
+input wire	BranchTaken;
+input wire	IF_ID_WriteEnable;
+input wire	clk_ROM;
+input wire	[7:0] BranchTarget;
+output wire	[31:0] IF_ID_Instruction;
+output wire	[7:0] IF_ID_PC;
 
-    // Somador do PC
-    // Incrementa o valor do PC em +1 (ou +4 dependendo da arquitetura) para buscar a próxima instrução.
-    PC_Adder pcAdder (
-        .inPC(currentPC),
-        .outPC(incrementedPC)
-    );
+wire	[7:0] SYNTHESIZED_WIRE_0;
+wire	[7:0] SYNTHESIZED_WIRE_6;
+wire	[7:0] SYNTHESIZED_WIRE_7;
+wire	[31:0] SYNTHESIZED_WIRE_3;
 
-    // MUX para seleção do próximo valor do PC
-    // Seleciona entre o endereço incrementado (incrementedPC) e o alvo de branch (branchTarget),
-    // com base no sinal 'PCSrc', que indica se um branch foi tomado.
-    Mux2to1 pcMux (
-        .in0(incrementedPC),     // Caminho normal: próxima instrução sequencial
-        .in1(branchTarget),      // Caminho alternativo: endereço de desvio
-        .sel(BranchTaken),       // Seleciona o próximo endereço do PC em caso de desvio
-        .out(nextPC)
-    );
 
-    // Registrador de pipeline IF/ID
-    // Armazena o valor do PC incrementado e a instrução buscada para o próximo estágio (ID).
-    // A escrita nos registradores é controlada por 'IF_ID_WriteEnable'.
-    IF_ID_Register if_id (
-        .clk(clk),
-        .rst(rst),                // Reset para limpar os registros
-        .inPC(incrementedPC),             // Valor do PC incrementado
-        .inInstruction(instruction),      // Instrução buscada da memória
-        .writeEnable(IF_ID_WriteEnable),  // Habilita a escrita
-        .outPC(IF_ID_PC),                 // Saída do PC para o próximo estágio
-        .outInstruction(IF_ID_Instruction) // Saída da instrução para o próximo estágio
-    );
+
+
+
+ProgramCounter	b2v_inst(
+	.clk(clk),
+	.rst(rst),
+	.nextPC(SYNTHESIZED_WIRE_0),
+	.currentPC(SYNTHESIZED_WIRE_7));
+
+
+Mux2to1	b2v_inst1(
+	.sel(BranchTaken),
+	.in0(SYNTHESIZED_WIRE_6),
+	.in1(BranchTarget),
+	.out(SYNTHESIZED_WIRE_0));
+
+
+PCAdder	b2v_inst2(
+	.inPC(SYNTHESIZED_WIRE_7),
+	.outPC(SYNTHESIZED_WIRE_6));
+
+
+IF_ID_Register	b2v_inst3(
+	.clk(clk),
+	.rst(rst),
+	.writeEnable(IF_ID_WriteEnable),
+	.inInstruction(SYNTHESIZED_WIRE_3),
+	.inPC(SYNTHESIZED_WIRE_6),
+	.outInstruction(IF_ID_Instruction),
+	.outPC(IF_ID_PC));
+
+
+ROM	b2v_inst4(
+	.clock(clk_ROM),
+	.address(SYNTHESIZED_WIRE_7),
+	.q(SYNTHESIZED_WIRE_3));
+
 
 endmodule
